3107e9b02c81d23df46d174cb4aa0655231bb403
Sreenu <Sreenu@192.168.0.28>
2017-04-04 19:14:11 -0500

    added files

---
+++ spring-gradle/src/main/java/com/controller/SpringController.java
@@ -0,0 +1,21 @@
+package com.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class SpringController {
+	
+	  @Value("${message}")
+	    private String message;
+	  
+	@RequestMapping(value="/greeting")
+	public String greetings()
+	{
+		return message + "test with Gradle";
+	}
+
+
+}
